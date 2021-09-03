c64dd6fb0b46f233c2b42c86982b664c90b6cc20
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-09-03 10:01:08 +0200

    doctrine

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,36 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/program", name="program_")
+ */
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render(
+            'program/index.html.twig',
+            ['website' => 'Wild Séries']
+        );
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render(
+            'program/show.html.twig',
+            ['program' => $id]
+        );
+    }
+}
